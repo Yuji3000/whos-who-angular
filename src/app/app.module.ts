461b5471ba03a6ffbda7e6ed1b066aa1cba98f1a
Mohamed Mohamed <therealmohamedmohamed@gmail.com>
2024-09-17 10:55:44 -0400

    Add example symbol

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,13 @@ import { RouterModule, Routes } from "@angular/router";
 import { AppComponent } from "./app.component";
 import { HomeComponent } from "./home/home.component";
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import {MatIcon} from "@angular/material/icon";
 
 const routes: Routes = [{ path: "", component: HomeComponent }];
 
 @NgModule({
   declarations: [AppComponent, HomeComponent],
-  imports: [BrowserModule, FormsModule, RouterModule.forRoot(routes)],
+  imports: [BrowserModule, FormsModule, RouterModule.forRoot(routes), MatIcon],
   providers: [
     provideAnimationsAsync()
   ],
