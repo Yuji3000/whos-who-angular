45fd89263159f718ad55a8d7d00fef81edaa8351
Yuji Kosakowski <yuji.k.dev@gmail.com>
2024-09-17 11:13:14 -0600

    Navbar working

--- src/app/Components/navbar/navbar.component.ts
+++ src/app/Components/navbar/navbar.component.ts
@@ -1,13 +1,29 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterOutlet, RouterLink } from '@angular/router';
+import { HomeComponent } from 'src/app/home/home.component';
+import { LeaderboardComponent } from 'src/app/leaderboard/leaderboard.component';
+import { SettingsComponent } from 'src/app/settings/settings.component';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [ CommonModule],
+  imports: [
+    CommonModule,
+    RouterOutlet,
+    HomeComponent,
+    NavbarComponent, 
+    SettingsComponent, 
+    LeaderboardComponent, 
+    RouterLink,
+  ],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css'
 })
+
 export class NavbarComponent {
 
 }
+
+
+
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,12 @@
 import { Component } from "@angular/core";
-import {RouterOutlet} from "@angular/router";
-import {HomeComponent} from "./home/home.component";
-
+import { NavbarComponent } from "./Components/navbar/navbar.component";
+import { MatButtonModule } from '@angular/material/button';
 @Component({
   selector: "app-root",
   templateUrl: "./app.component.html",
   styleUrls: ["./app.component.css"],
   standalone: true,
-  imports: [RouterOutlet, HomeComponent]
+  imports: [NavbarComponent, MatButtonModule],
 })
 export class AppComponent {
   title: String = "angular-whos-who";
